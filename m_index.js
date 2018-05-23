220f968024d644574f97235f0b4ebb04aede49e0
Steve <steveucho@nyu.edu>
2018-05-22 20:01:01 -0400

    added jQuery on new page

--- m_index.js
+++ m_index.js
@@ -1,6 +1,17 @@
 // const electron = require('electron');
 const remot = require('electron').remote;
 
+function createWindow(file){
+    newWindow = new remot.BrowserWindow({
+        title: 'new Window!',
+        titleBarStyle: 'hidden',
+        backgroundColor: '#FFF',
+        center: true,
+        opacity: 1
+    });
+    newWindow.loadFile(file);
+}
+
 let list = document.getElementById('maintain');
 var li = document.createElement('li');
 let openDev = document.createElement('button');
@@ -14,8 +25,8 @@ li.appendChild(openDev);
 list.appendChild(li);
 
 var li = document.createElement('li');
-
 let refresh = document.createElement('button');
+
 refresh.textContent = "Refresh";
 refresh.addEventListener('click', function(){
     let win = remot.getCurrentWindow();
@@ -24,3 +35,10 @@ refresh.addEventListener('click', function(){
 });
 li.appendChild(refresh);
 list.appendChild(li);
+
+let newWin = document.getElementById('new-window');
+newWin.addEventListener('click', function(){
+    // let win = new remot.BrowserWindow()
+    // win.loadFile('./html/createJson.html')
+    createWindow('./html/createModel.html')
+})
