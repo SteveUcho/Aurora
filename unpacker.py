e22d046c0d26e6b8c6e42c652956d2e608ecca48
Steve <steveucho@nyu.edu>
2018-06-02 11:42:36 -0400

    attempting to make drag and drop nodes

--- unpacker.py
+++ unpacker.py
@@ -5,12 +5,12 @@ def unpackJson():
     with open('myfile.json', encoding='utf-8') as data_file:
         data = json.loads(data_file.read())
         pprint(data)
-        print("Parsing each one individually...")
-        for i in range(0, len(data)):
-            dataParsed = data[str(i)]
-            print("Printing a dictionary...")
-            print(dataParsed)
-            print("Printing dic['name'] in order to get the type")
-            print(dataParsed["name"])
+        # print("Parsing each one individually...")
+        # for i in range(0, len(data)):
+        #     dataParsed = data[str(i)]
+        #     print("Printing a dictionary...")
+        #     print(dataParsed)
+        #     print("Printing dic['name'] in order to get the type")
+        #     print(dataParsed["name"])
 
 unpackJson()
